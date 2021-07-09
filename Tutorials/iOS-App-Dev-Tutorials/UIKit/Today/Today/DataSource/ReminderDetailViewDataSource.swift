083ea676f48988ab9a4dfa4d208e5ef90634397f
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-07-10 03:51:01 +0900

    Format Dates Displayed in Cells

--- Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/DataSource/ReminderDetailViewDataSource.swift
+++ Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/DataSource/ReminderDetailViewDataSource.swift
@@ -14,14 +14,30 @@ class ReminderDetailViewDataSource: NSObject {
         case time
         case notes
         
+        static let timeFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .none
+            formatter.timeStyle = .short
+            return formatter
+        }()
+        
+        static let dateFormatter: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.timeStyle = .none
+            formatter.dateStyle = .long
+            return formatter
+        }()
+        
         func displayText(for reminder: Reminder?) -> String? {
             switch self {
             case .title:
                 return reminder?.title
             case .date:
-                return reminder?.dueDate.description
+                guard let date = reminder?.dueDate else { return nil }
+                return Self.dateFormatter.string(from: date)
             case .time:
-                return reminder?.dueDate.description
+                guard let date = reminder?.dueDate else { return nil }
+                return Self.timeFormatter.string(from: date)
             case .notes:
                 return reminder?.notes
             }
--- Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/DataSource/ReminderListDataSource.swift
+++ Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/DataSource/ReminderListDataSource.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ReminderListDataSource: NSObject {
-    
+    private lazy var dateFormatter = RelativeDateTimeFormatter()
 
 }
 
@@ -24,15 +24,8 @@ extension ReminderListDataSource: UITableViewDataSource {
             fatalError("Unable to dequeue ReminderCell")
         }
         let reminder = Reminder.testData[indexPath.row]
-        //        let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
-        //        cell.titleLabel.text = reminder.title
-        //        cell.dateLabel.text = reminder.dueDate.description
-        //        cell.doneButton.setBackgroundImage(image, for: .normal)
-        //        cell.doneButtonAction = {
-        //            Reminder.testData[indexPath.row].isComplete.toggle()
-        //            tableView.reloadRows(at: [indexPath], with: .none)
-        //        }
-        cell.configure(title: reminder.title, dateText: reminder.dueDate.description, isDone: reminder.isComplete) {
+        let dateText = dateFormatter.localizedString(for: reminder.dueDate, relativeTo: Date())
+        cell.configure(title: reminder.title, dateText: dateText, isDone: reminder.isComplete) {
             Reminder.testData[indexPath.row].isComplete.toggle()
             tableView.reloadRows(at: [indexPath], with: .none)
         }
