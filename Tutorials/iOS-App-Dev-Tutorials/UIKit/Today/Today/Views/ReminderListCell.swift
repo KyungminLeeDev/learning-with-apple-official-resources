c465b5a632524d25b0086668cd20d48703bd9d11
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-07-10 01:11:51 +0900

    Create Outlets in Interface Builder

---
+++ Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/Views/ReminderListCell.swift
@@ -0,0 +1,29 @@
+//
+//  ReminderListCell.swift
+//  Today
+//
+//  Created by Kyungmin Lee on 2021/07/10.
+//
+
+import UIKit
+
+class ReminderListCell: UITableViewCell {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
+    
+    
+    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
