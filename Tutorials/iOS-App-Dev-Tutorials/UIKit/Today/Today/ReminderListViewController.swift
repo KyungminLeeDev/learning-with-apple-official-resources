c0a2895a29e18f811d04998669619a98269b5c67
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-07-10 01:39:16 +0900

    Add Data Source Methods

--- Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/ReminderListViewController.swift
+++ Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/ReminderListViewController.swift
@@ -12,3 +12,26 @@ class ReminderListViewController: UITableViewController {
 
 }
 
+extension ReminderListViewController {
+    static let reminderListCellIdentifier = "ReminderListCell"
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Reminder.testData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
+            fatalError("Unable to dequeue ReminderCell")
+        }
+        let reminder = Reminder.testData[indexPath.row]
+        let image = reminder.isComplete ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
+        cell.titleLabel.text = reminder.title
+        cell.dateLabel.text = reminder.dueDate.description
+        cell.doneButton.setBackgroundImage(image, for: .normal)
+        cell.doneButtonAction = {
+            Reminder.testData[indexPath.row].isComplete.toggle()
+            tableView.reloadRows(at: [indexPath], with: .none)
+        }
+        return cell
+    }
+}
