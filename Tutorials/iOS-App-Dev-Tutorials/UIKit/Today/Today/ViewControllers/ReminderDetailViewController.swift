76b1907d960c32af65370377e722494a6d6a9d56
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-07-10 03:37:05 +0900

    Update the Detail View Controller

--- Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/ViewControllers/ReminderDetailViewController.swift
+++ Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/ViewControllers/ReminderDetailViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ReminderDetailViewController: UITableViewController {
     
-    var reminder: Reminder?
+    private var reminder: Reminder?
+    private var detailViewDataSource: ReminderDetailViewDataSource?
     
     func configure(with reminder: Reminder) {
         self.reminder = reminder
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let reminder = reminder else {
+            fatalError("No reminder found for detail view")
+        }
+        detailViewDataSource = ReminderDetailViewDataSource(reminder: reminder)
+        tableView.dataSource = detailViewDataSource
     }
 }
