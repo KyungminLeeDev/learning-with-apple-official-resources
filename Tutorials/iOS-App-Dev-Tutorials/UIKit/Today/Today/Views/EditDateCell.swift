206df59a0b231ead2864b6fe68e1415836b00359
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-07-13 16:18:11 +0900

    Editable Cells - Editing Reminders

--- Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/Views/EditDateCell.swift
+++ Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/Views/EditDateCell.swift
@@ -8,9 +8,25 @@
 import UIKit
 
 class EditDateCell: UITableViewCell {
+    typealias DateChangeAction = (Date) -> Void
+    
     @IBOutlet var datePicker: UIDatePicker!
+    
+    private var dateChangeAction: DateChangeAction?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        datePicker.addTarget(self, action: #selector(dateChanged(_:)), for: .valueChanged)
+    }
 
-    func configure(date: Date) {
+    func configure(date: Date, changeAction: @escaping DateChangeAction) {
         datePicker.date = date
+        self.dateChangeAction = changeAction
+    }
+    
+    @objc
+    func dateChanged(_ sender: UIDatePicker) {
+        dateChangeAction?(sender.date)
     }
 }
