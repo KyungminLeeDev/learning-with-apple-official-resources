34f23cf39e36db8dbacd5a26b58d99fa7d38cb81
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-07-14 13:49:16 +0900

    Filtering Data - Working with a Dynamic Data Source

--- Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/ReminderListViewController.swift
+++ Tutorials/iOS-App-Dev-Tutorials/UIKit/Today/Today/ReminderListViewController.swift
@@ -30,7 +30,7 @@ class ReminderListViewController: UITableViewController {
             }
             destination.configure(with: reminder, editAction:  { reminder in
                 self.reminderListDataSource?.update(reminder, at: rowIndex)
-                self.tableView.reloadRows(at: [indexPath], with: .automatic)
+                self.tableView.reloadData()
             })
         }
     }
@@ -61,10 +61,11 @@ class ReminderListViewController: UITableViewController {
     private func addReminder() {
         let storyboard = UIStoryboard(name: Self.mainStoryboardName, bundle: nil)
         let detailViewController: ReminderDetailViewController = storyboard.instantiateViewController(identifier: Self.detailViewControllerIdentifier)
-        let reminder = Reminder(title: "New Reminder", dueDate: Date())
+        let reminder = Reminder(id: UUID().uuidString, title: "New Reminder", dueDate: Date())
         detailViewController.configure(with: reminder, isNew: true, addAction: { reminder in
-            self.reminderListDataSource?.add(reminder)
-            self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
+            if let index = self.reminderListDataSource?.add(reminder) {
+                self.tableView.insertRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+            }
         })
         let navigationController = UINavigationController(rootViewController: detailViewController)
         present(navigationController, animated: true, completion: nil)
