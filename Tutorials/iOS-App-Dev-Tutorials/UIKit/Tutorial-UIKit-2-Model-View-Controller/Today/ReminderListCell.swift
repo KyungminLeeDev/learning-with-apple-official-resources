006349664926cfb95cc7cd529a6a0e89dd03668c
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-08-02 16:11:25 +0900

    [#4] Add Actions Programmatically
    Connecting Outlets and Actions
    - 2. Add Actions Programmatically

--- Tutorials/iOS-App-Dev-Tutorials/UIKit/Tutorial-UIKit-2-Model-View-Controller/Today/ReminderListCell.swift
+++ Tutorials/iOS-App-Dev-Tutorials/UIKit/Tutorial-UIKit-2-Model-View-Controller/Today/ReminderListCell.swift
@@ -8,8 +8,15 @@
 import UIKit
 
 class ReminderListCell: UITableViewCell {
+    typealias DoneButtonAction = () -> Void
+    
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var doneButton: UIButton!
     
+    var doneButtonAction: DoneButtonAction?
+    
+    @IBAction func doneButtonTriggered(_ sender: UIButton) {
+        doneButtonAction?()
+    }
 }
