ebbb3790a885c4fcb9c6d37ceb3479cecdaecd2f
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-08-06 11:27:29 +0900

    [#10] Initial commit

---
+++ Tutorials/iOS-App-Dev-Tutorials/UIKit/Tutorial-UIKit-5-Editable-Cells/Today/ReminderListDataSource.swift
@@ -0,0 +1,33 @@
+//
+//  ReminderListDataSource.swift
+//  Today
+//
+//  Created by Kyungmin Lee on 2021/08/05.
+//
+
+import UIKit
+
+class ReminderListDataSource: NSObject {
+    private lazy var dateFormatter = RelativeDateTimeFormatter()
+}
+
+extension ReminderListDataSource: UITableViewDataSource {
+    static let reminderListCellIdentifier = "ReminderListCell"
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Reminder.testData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Self.reminderListCellIdentifier, for: indexPath) as? ReminderListCell else {
+            fatalError("Unable to dequeue ReminderCell")
+        }
+        let reminder = Reminder.testData[indexPath.row]
+        let dateText = dateFormatter.localizedString(for: reminder.dueDate, relativeTo: Date())
+        cell.configure(title: reminder.title, dateText: dateText, isDone: reminder.isComplete) {
+            Reminder.testData[indexPath.row].isComplete.toggle()
+            tableView.reloadRows(at: [indexPath], with: .none)
+        }
+        return cell
+    }
+}
---
+++ Tutorials/iOS-App-Dev-Tutorials/UIKit/Tutorial-UIKit-5-Editable-Cells/Today/ReminderListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Kyungmin Lee on 2021/07/20.
+//
+
+import UIKit
+
+class ReminderListViewController: UITableViewController {
+    
+    private var reminderListDataSource: ReminderListDataSource?
+    
+    static let showDetailSegueIdentifier = "ShowReminderDetailSegue"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Self.showDetailSegueIdentifier,
+           let destination = segue.destination as? ReminderDetailViewController,
+           let cell = sender as? UITableViewCell,
+           let indexPath = tableView.indexPath(for: cell) {
+            let reminder = Reminder.testData[indexPath.row]
+            destination.configure(with: reminder)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        reminderListDataSource = ReminderListDataSource()
+        tableView.dataSource = reminderListDataSource
+    }
+}
